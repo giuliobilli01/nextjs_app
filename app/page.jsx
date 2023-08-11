65ea388a3fa994b18792e47ff5a95c54477a6f2b
Giulio Billi <72358159+giuliobilli01@users.noreply.github.com>
2023-08-11 22:52:33 +0200

    chore: navbar setup and project skeleton

---
+++ app/page.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Feed from "@components/Feed";
+
+function Home() {
+  return (
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Discover & Share
+        <br />
+        <span className="orange_gradient text-center">AI-Powered Prompts</span>
+      </h1>
+      <p className="desc text-center">
+        Promptopia is an open-source AI prompting tool for modern world to
+        discover
+      </p>
+
+      <Feed></Feed>
+    </section>
+  );
+}
+
+export default Home;
