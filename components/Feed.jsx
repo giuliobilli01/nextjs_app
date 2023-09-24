21ef42be5d11c8ac1df7c4fd49798e626cb1f689
Giulio Billi <72358159+giuliobilli01@users.noreply.github.com>
2023-09-24 17:19:11 +0200

    chore: update

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,7 +1,50 @@
+"use client";
 import React from "react";
+import { useState, useEffect } from "react";
+import PromptCard from "./PromptCard";
+
+const PromptCardList = ({ data, handleTagClick }) => {
+  return (
+    <div className="mt-16 prompt_lay">
+      {data.map((post) => (
+        <PromptCard
+          key={post._id}
+          post={post}
+          handleTagClick={handleTagClick}
+        ></PromptCard>
+      ))}
+    </div>
+  );
+};
 
 const Feed = () => {
-  return <div>Feed</div>;
+  const [searchText, setSearchText] = useState("");
+  const [posts, setPosts] = useState([]);
+
+  const handleSearchChange = () => {};
+
+  useEffect(() => {
+    const fetchPost = async () => {
+      const response = await fetch("/api/prompt");
+      const data = await response.json();
+    };
+  });
+  return (
+    <section className="feed">
+      <form className="relative w-full flex-center">
+        <input
+          type="text"
+          placeholder="Search for prompts"
+          value={searchText}
+          onChange={handleSearchChange}
+          required
+          className="search_input peer"
+        ></input>
+      </form>
+
+      <PromptCardList data={posts} handleTagClick={() => {}}></PromptCardList>
+    </section>
+  );
 };
 
 export default Feed;
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -5,7 +5,7 @@ import { useState, useEffect } from "react";
 import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
 const Nav = () => {
-  const isUserLoggedIn = true;
+  const { data: session } = useSession();
   const [providers, setProviders] = useState(null);
   const [toggleDropdown, setToggleDropdown] = useState(false);
 
@@ -30,9 +30,8 @@ const Nav = () => {
         ></Image>
         <p className="logo_text">Promptopia</p>
       </Link>
-
       <div className="sm:flex hidden">
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className="flex gap-3 md:gap-5">
             <Link href="/create-prompt" className="black_btn">
               Create Post
@@ -42,7 +41,7 @@ const Nav = () => {
             </button>
             <Link href="/profile">
               <Image
-                src="/assets/icons/user.png"
+                src={session?.user.image}
                 width={37}
                 height={37}
                 className="rounded-full"
@@ -54,14 +53,16 @@ const Nav = () => {
           <>
             {providers &&
               Object.values(providers).map((provider) => {
-                <button
-                  type="button"
-                  key={provider.name}
-                  onClick={() => signIn(provider.id)}
-                  className="black_btn"
-                >
-                  Sign In
-                </button>;
+                return (
+                  <button
+                    type="button"
+                    key={provider.name}
+                    onClick={() => signIn(provider.id)}
+                    className="black_btn"
+                  >
+                    Sign In
+                  </button>
+                );
               })}
           </>
         )}
@@ -69,7 +70,7 @@ const Nav = () => {
 
       {/* Mobile Navigation */}
       <div className="sm:hidden flex relative">
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className="flex">
             <button
               onClick={() => {
